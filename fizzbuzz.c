4c1fbfd8704025d2488a301e4e8d907e26563740
dct2012 <cameron.taaffe@gmail.com>
2015-07-22 16:10:36 -0500

    first

---
+++ fizzbuzz.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    for(int i = 1; i <= 100; i++)
+    {
+        ((i % 3 || !printf("Fizz")) & (i % 5 || !printf("Buzz"))) && printf("%d", i);
+        printf("\n");
+    }
+}
