c5a394484bbda71e53e48372709ff1fbad2c3f6f
Cameron Taaffe <cameron.taaffe@gmail.com>
2015-07-22 16:16:16 -0500

    more code golfed

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -3,8 +3,5 @@
 int main()
 {
     for(int i = 1; i <= 100; i++)
-    {
-        ((i % 3 || !printf("Fizz")) & (i % 5 || !printf("Buzz"))) && printf("%d", i);
-        printf("\n");
-    }
+        (((i % 3 || !printf("Fizz")) & (i % 5 || !printf("Buzz"))) && printf("%d", i)) & printf("\n");
 }
